855d73269ecb65e424db76e136dc723be87332c0
Duc Nguyen <minhduc140583@gmail.com>
2021-12-12 22:50:55 +0700

    Add repository

--- src/user/index.ts
+++ src/user/index.ts
@@ -1,15 +1,22 @@
 import { Db } from 'mongodb';
 import { buildQuery, SearchBuilder } from 'mongodb-extension';
-import { User, UserFilter, userModel, UserService } from './user';
+import { Manager, SearchResult } from 'onecore';
+import { User, UserFilter, userModel, UserRepository, UserService } from './user';
 import { UserController } from './user-controller';
 export * from './user';
 export { UserController };
 
-import { MongoUserService } from './mongo-user-service';
+import { MongoUserRepository } from './mongo-user-repository';
 
+export class UserManager extends Manager<User, string, UserFilter> implements UserService {
+  constructor(find: (s: UserFilter, limit?: number, offset?: number | string, fields?: string[]) => Promise<SearchResult<User>>, repository: UserRepository) {
+    super(find, repository);
+  }
+}
 export function useUser(db: Db): UserService {
   const builder = new SearchBuilder<User, UserFilter>(db, 'users', buildQuery, userModel.attributes);
-  return new MongoUserService(builder.search, db);
+  const repository = new MongoUserRepository(db);
+  return new UserManager(builder.search, repository);
 }
 export function useUserController(log: (msg: string) => void, db: Db): UserController {
   return new UserController(log, useUser(db));
